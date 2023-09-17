f453473a46821853fa7be2f636ac4b97d02c0591
David <github@dankme.me.uk>
2023-09-17 22:24:57 +0200

    Initial

---
+++ Skatteberegner/Program.cs
@@ -0,0 +1,10 @@
+﻿    public class Beregning {
+        static void Main(string[] args) {
+
+        }
+        
+        // Funktionen returnerer det beløb, der skal beskattes (altså ikke selve skatten).
+        public double SkatVedJulegave(double julegave, double andenGave) {
+            return 0;
+        }
+    }
